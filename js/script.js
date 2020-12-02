3bc8b5b03d1519c8f6e3b1ea2649c3b7a0356ce6
Roberto <rdrago.uni@gmail.com>
2020-12-02 17:58:54 +0100

    next page button working

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
-var filmApi = 'https://api.themoviedb.org/3/search/movie?api_key=52d4b96c01f3627af936fa42e8430298&query='
+var filmApi = 'https://api.themoviedb.org/3/search/movie?api_key=52d4b96c01f3627af936fa42e8430298&query=';
+var pageApi = '&page='
 
 var app = new Vue ({
     el: '#app',
     data: {
        filmArray: [],
-       filmSelect: ''
+       filmSelect: '',
+       filmPage: 0,
+       filmPageTotal: 0,
+       filmTotal: 0,
+       counter: 1
     },
 
     methods: {
 
         searchFilm: function() {
-            filmApi = filmApi + this.filmSelect;
-            console.log(filmApi);
+            filmApi = 'https://api.themoviedb.org/3/search/movie?api_key=52d4b96c01f3627af936fa42e8430298&query=';
+            filmApi = filmApi + this.filmSelect + pageApi + '1';
             axios.get(filmApi)
             .then(film => {
                 this.filmArray = film.data.results;
+                this.filmPage = film.data.page;
+                this.filmPageTotal = film.data.total_pages;
+                this.filmTotal = film.data.total_results;
             })
-            filmApi = 'https://api.themoviedb.org/3/search/movie?api_key=52d4b96c01f3627af936fa42e8430298&query=';
+        },
+
+        nextPage: function() {
+            filmApi = filmApi + this.filmSelect + pageApi + (++this.counter);
+            axios.get(filmApi)
+            .then(film => {
+                this.filmArray = film.data.results;
+                this.filmPage = film.data.page;
+                this.filmPageTotal = film.data.total_pages;
+                this.filmTotal = film.data.total_results;
+            })
         }
 
     }
